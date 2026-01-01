6dacfb9b1af4374ad74c26fb72dbfc43642419c0
jackofall1232 <jackofall1232@gmail.com>
2026-01-01 11:39:36 -0800

    Bump version from 1.0.0 to 0.3.0

--- crypto-miner-tycoon.php
+++ crypto-miner-tycoon.php
@@ -3,7 +3,7 @@
  * Plugin Name: Crypto Miner Tycoon
  * Plugin URI: https://cryptonerd.com
  * Description: An engaging crypto-themed idle clicker game with Elo-balanced progression. Use shortcode [crypto_miner_tycoon] to display the game.
- * Version: 1.0.0
+ * Version: 0.3.0
  * Author: Joe
  * Author URI: https://cryptonerd.com
  * License: GPL v2 or later
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('CMT_VERSION', '1.0.0');
+define('CMT_VERSION', '0.3.0');
 define('CMT_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CMT_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('CMT_PLUGIN_BASENAME', plugin_basename(__FILE__));
